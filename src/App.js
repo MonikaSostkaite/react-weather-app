abb820186fd4a0d9d37ac86542325e2f4d599b8b
Monika Šostkaitė <Monika.Sostkaite@adform.com>
2021-03-07 19:58:25 +0200

    Fetch weather data by current location; Add AppLayout view;

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { isEmpty } from "lodash";
+import keys from "./apiKeys";
+import logo from './media/weather-forecast-vector.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.scss';
+
+const api = {
+    key: keys.API_KEY,
+    base: keys.BASE_URL,
+};
+
+const AppLayout = () => {
+    const [weather, setWeather] = useState({});
+    const [latitude, setLatitude] = useState("");
+    const [longitude, setLongitude] = useState("");
+
+    useEffect(() => {
+        window.navigator.geolocation.getCurrentPosition(async position => {
+            setLatitude(position.coords.latitude);
+            setLongitude(position.coords.longitude);
+        });
+    }, [latitude, longitude]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await fetch(`${api.base}onecall?lat=${latitude}&lon=${longitude}&units=metric&APPID=${api.key}`)
+            .then((res) => res.json())
+            .then((result) => {
+                setWeather(result);
+            });
+        }
+
+        // call the async fetchData function
+        if (latitude && longitude) fetchData();
+    }, [latitude, longitude]);
+    
+    return (
+        <div id="page">
+            <header>
+                {!isEmpty(weather) ? (
+                    <section>
+                        <img src={logo} alt="My logo" />
+                        <h1 className="h1-title">
+                            <span>Weather in</span>
+                            <p>{weather.timezone}</p>
+                        </h1>
+                        <div>
+                            <span>{weather.current.temp}<sup>o</sup>C</span>
+                        </div>
+                    </section>
+                ) : (
+                    <div>no weather for that location</div>
+                )}
+            </header>
+            <main>
+                
+            </main>
+            <footer></footer>
+        </div>
+    );
 }
 
-export default App;
+export default AppLayout;
---
+++ src/apiKeys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    API_KEY: "fab44028d3265297ca403a010dab777f",
+    BASE_URL: "https://api.openweathermap.org/data/2.5/"
+}
